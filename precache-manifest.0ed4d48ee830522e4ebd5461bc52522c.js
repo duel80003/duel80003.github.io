e30c83de7b105aa5b059817137d1f48910007b65
bill hong <duel80003@gmail.com>
2020-02-19 22:31:49 +0800

    add index

---
+++ precache-manifest.0ed4d48ee830522e4ebd5461bc52522c.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82795d7fc278bba4829a",
+    "url": "/css/app.f9d013c4.css"
+  },
+  {
+    "revision": "8b5ebf52f209b6660d39",
+    "url": "/css/chunk-vendors.e21a7be9.css"
+  },
+  {
+    "revision": "5073ed91b5c38de06d682cdee7c8adcd",
+    "url": "/fonts/Eva-Icons.5073ed91.woff2"
+  },
+  {
+    "revision": "f8715d9989eadd3dfa6181d8725a4820",
+    "url": "/fonts/Eva-Icons.f8715d99.woff"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/fonts/KFOkCnqEu92Fr1MmgVxIIzQ.5cb7edfc.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/fonts/KFOlCnqEu92Fr1MmEU9fBBc-.87284894.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/fonts/KFOlCnqEu92Fr1MmSU5fBBc-.b00849e0.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/fonts/KFOlCnqEu92Fr1MmWUlfBBc-.adcde98f.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bb1e4dc6.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/fonts/KFOmCnqEu92Fr1Mu4mxM.60fa3c06.woff"
+  },
+  {
+    "revision": "d103b56582b78f5ef84cc644a557701f",
+    "url": "/fonts/LDItaoyNOAY6Uewc665JcIzCKsKc_M9flwmJ.d103b565.woff"
+  },
+  {
+    "revision": "e6f6ee20b5266ea64d746970667d97fa",
+    "url": "/fonts/LDItaoyNOAY6Uewc665JcIzCKsKc_M9flwmPq_HTTw.e6f6ee20.woff2"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/fonts/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/fonts/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/fonts/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/fonts/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/fonts/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/fonts/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "76f5a1109e15c12d93bfa14bf603e554",
+    "url": "/fonts/gok-H7zzDkdnRel8-DQ6KAXJ69wP1tGnf4ZGhUcY.76f5a110.woff"
+  },
+  {
+    "revision": "ab5291b73aeaede4da98f0f2e4267bca",
+    "url": "/fonts/gok-H7zzDkdnRel8-DQ6KAXJ69wP1tGnf4ZGhUcel5euIg.ab5291b7.woff2"
+  },
+  {
+    "revision": "96f1c901c087fb64019f7665f7f8aca6",
+    "url": "/fonts/ionicons.96f1c901.woff2"
+  },
+  {
+    "revision": "99b863497156d4478ec3489fefb3815c",
+    "url": "/fonts/ionicons.99b86349.woff"
+  },
+  {
+    "revision": "043774d1de6da96b11c8ade1a61c60cb",
+    "url": "/fonts/materialdesignicons-webfont.043774d1.woff"
+  },
+  {
+    "revision": "27cb2cf143cf204f7cfc210089fd10e8",
+    "url": "/fonts/materialdesignicons-webfont.27cb2cf1.woff2"
+  },
+  {
+    "revision": "05aced73724283a909dfaf04135878b6",
+    "url": "/fonts/oPWQ_lt5nv4pWNJpghLP75WiFR4kLh3kvmvRImcycg.05aced73.woff2"
+  },
+  {
+    "revision": "fb2bd6c71d16280966700d93254bbc9a",
+    "url": "/fonts/oPWQ_lt5nv4pWNJpghLP75WiFR4kLh3kvmvX.fb2bd6c7.woff"
+  },
+  {
+    "revision": "88a94f91a47748608b7293e8fdbd5c50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93e00c6672881009c645",
+    "url": "/js/about.debb100d.js"
+  },
+  {
+    "revision": "82795d7fc278bba4829a",
+    "url": "/js/app.c0f91c96.js"
+  },
+  {
+    "revision": "8b5ebf52f209b6660d39",
+    "url": "/js/chunk-vendors.9a0f8174.js"
+  },
+  {
+    "revision": "186023ce050cf42c68fedc24bc11c57f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
